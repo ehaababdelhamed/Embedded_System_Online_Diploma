3429a57211399c3a12c132454028acf28e41c235
Ehab Abdelhamed <ehababdelhamed139@gmail.com>
2023-07-07 03:58:48 +0300

    Ex1

---
+++ Unit2_C_Programming/C_Basics_Assigments/Assigment_1/EX1_PrintStatement.c
@@ -0,0 +1,16 @@
+/******************************************************************************
+ * Uint2     : Lesson3_C Basics >> Assignment1
+ * File Name : EX1_PrintStatement.c
+ * Data      : 6/7/2023
+ * Author    : Ehab Mohamed Abdelhmaed
+ *****************************************************************************/
+#include <stdio.h>
+
+int main(){
+
+	printf("C Programming");
+
+	return 0;
+}
+
+
