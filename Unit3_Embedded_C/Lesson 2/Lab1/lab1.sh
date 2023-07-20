c25828cc471f91aab46a249fa9ec11e4f7b03885
Ehab Abdelhamed <ehababdelhamed139@gmail.com>
2023-07-20 19:54:44 +0300

    Add Embedded C Lab 1

--- Unit3_Embedded_C/Lesson 2/Lab1/lab1.sh
+++ Unit3_Embedded_C/Lesson 2/Lab1/lab1.sh
@@ -1,12 +1,33 @@
 #!/bin/bash 
 
+echo "Comlpliation...... >> files.o"
 #Compilation 
 arm-none-eabi-gcc -c -mcpu=arm926ej-s uart.c -o uart.o
 arm-none-eabi-gcc -c -mcpu=arm926ej-s app.c  -o  app.o
 arm-none-eabi-gcc -c -mcpu=arm926ej-s startup.s -o startup.o
 
+echo "Linking...... >> file.elf"
 #Linking
  arm-none-eabi-ld -T linker_script.ld app.o uart.o startup.o -o learn-in-depth.elf -Map=Map_file.map
+ 
+: '
+#Sections of files
+arm-none-eabi-objdump.exe -h uart.o 
+arm-none-eabi-objdump.exe -h app.o 
+arm-none-eabi-objdump.exe -h startup.o 
+arm-none-eabi-objdump.exe -h learn-in-depth.elf
+'
+#Symbols of files
+echo "uart.o symbols"
+arm-none-eabi-nm  uart.o 
+echo "app.o symbols"
+arm-none-eabi-nm  app.o
+echo "startup.o symbols"
+arm-none-eabi-nm  startup.o  
+echo "learn-in-depth.elf symbols"
+arm-none-eabi-nm  learn-in-depth.elf 
+
+echo "objcopy...... >> file.bin"
 
 #binary file
 arm-none-eabi-objcopy.exe -O binary learn-in-depth.elf learn-in-depth.bin 
@@ -14,19 +35,6 @@ arm-none-eabi-objcopy.exe -O binary learn-in-depth.elf learn-in-depth.bin
 #Simulation
  qemu-system-arm.exe -M versatilepb -m 128M -nographic -kernel learn-in-depth.bin
 
-: '
- #Sections of files
-arm-none-eabi-objdump.exe -h uart.o 
-arm-none-eabi-objdump.exe -h app.o 
-arm-none-eabi-objdump.exe -h startup.o 
-arm-none-eabi-objdump.exe -h learn-in-depth.elf
-
-#Symbols of files
-arm-none-eabi-nm  uart.o 
-arm-none-eabi-nm  app.o 
-arm-none-eabi-nm  startup.o  
-arm-none-eabi-nm  learn-in-depth.elf #
-'
 
 
 
